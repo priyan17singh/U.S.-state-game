70e23b8eb3f6d993ff8426fed888cbb0a47b37b0
Priyanshu Singh <priyan17singh@gmail.com>
2025-06-29 12:52:14 +0530

    Using list compherence to reduce lines of code.

--- main.py
+++ main.py
@@ -19,10 +19,7 @@ while len(gussed_states) < 50:
     answer_state = turtle.textinput(title=f"{len(gussed_states)}/50 state correct", prompt="What's another state name").title()
 # player wants to exit the game.
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in gussed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in gussed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv") #The states you have missed.
         break
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,3 @@
-    answer_state = turtle.textinput(title="Guess the state", prompt="What's another state name").title()
-
-    # If answer state is one of the states in 50_states.csv.
-    if answer_state in all_states:
-        x = data.x[data.state == answer_state]
-        y = data.y[data.state == answer_state]
-        print(x)
-        t= turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        t.goto(int(x),int(y))
-        t.write(arg=answer_state, align="center", font=("Arial", 8, "normal"))
+for state in all_states:
+        #     if state not in gussed_states:
+        #         missing_states.append(state)
